758a5e5c2c6694949c008035703cf4a6773e62f1
wejdan al-subhi <alwejdan97@gmail.com>
2026-06-07 16:38:16 +0400

    create constructor for Product class

--- Demo/src/main/java/ProductApp/Demo/Entities/EventRegistration.java
+++ Demo/src/main/java/ProductApp/Demo/Entities/EventRegistration.java
@@ -3,4 +3,11 @@ package ProductApp.Demo.Entities;
 public class EventRegistration {
     private String registrationId;
     private String attendeeName;
+
+    public EventRegistration(String registrationId, String attendeeName) {
+        this.registrationId = registrationId;
+        this.attendeeName = attendeeName;
+    }
+
+
 }
