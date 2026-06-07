72be09af2f67c8b7b3fb1d8969ce1a1ef9beae2c
wejdan al-subhi <alwejdan97@gmail.com>
2026-06-07 16:50:38 +0400

    create methods for adding and deleting

--- Demo/src/main/java/ProductApp/Demo/Services/RegistrationService.java
+++ Demo/src/main/java/ProductApp/Demo/Services/RegistrationService.java
@@ -2,7 +2,9 @@ package ProductApp.Demo.Services;
 
 import ProductApp.Demo.Entities.EventRegistration;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class RegistrationService {
@@ -12,5 +14,24 @@ public class RegistrationService {
         registrations.put(101, new EventRegistration("E1-01", "Ahmed"));
         registrations.put(102, new EventRegistration("E2-02", "Sara"));
         registrations.put(103, new EventRegistration("E3-03", "John"));
+    }
+
+    public String deleteMultipleByIds(List<Integer> ids){
+        List<Integer> deletedIds=new ArrayList<>();
+        List<Integer> notFoundIds=new ArrayList<>();
+
+        for(Integer id:ids){
+            if(registrations.containsKey(id)){
+                registrations.remove(id);
+                deletedIds.add(id);
+            }else{
+                notFoundIds.add(id);
+            }
+        }
+        return "Deletion COMPLETED...\n"+
+                "Total Request: "+ ids.size()+"\n"+
+                "Total of Successful deleted: "+deletedIds.size()+"\n"+
+                "Deleted IDs: "+deletedIds.toString().replace("[","").replace("]","")+"\n"+
+                "NOT Found IDs: "+notFoundIds.toString().replace("[","").replace("]","");
     }
 }
