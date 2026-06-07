e0d824fb3d8ed28ff51ae73dec38905e070fb138
wejdan al-subhi <alwejdan97@gmail.com>
2026-06-07 16:28:01 +0400

    add new codes

--- Demo/src/main/java/ProductApp/Demo/Services/ProductService.java
+++ Demo/src/main/java/ProductApp/Demo/Services/ProductService.java
@@ -1,4 +1,29 @@
 package ProductApp.Demo.Services;
 
+import ProductApp.Demo.Entities.Product;
+
+import java.util.HashMap;
+
 public class ProductService {
+    HashMap<Integer, Product> productMap=new HashMap<>();
+
+    public void loadSampleProducts(){
+        productMap.put(1, new Product("P1-01", "Laptop", 500.0));
+        productMap.put(2, new Product("P2-02", "Phone", 300.0));
+        productMap.put(3, new Product("P3-03", "Tablet", 200.0));
+    }
+
+    public String deleteProductById(int productId){
+        if(!productMap.containsKey(productId)){
+            return productId+" NOT Found, NO Deletion Performed...";
+        }
+
+        Product productoToDelete=productMap.get(productId);
+        String deletedName=productoToDelete.getProductName();
+        productMap.remove(productId);
+        return "Product Deleted SUCCESSFULLT...\n"+
+                "Product ID: "+productId+"\n"+
+                "Product Name: "+deletedName;
+    }
+
 }
