5516095cecc8767f5b7e7d5b515b7fec7c66d48d
wejdan al-subhi <alwejdan97@gmail.com>
2026-06-07 16:42:01 +0400

    create HashMap RegistrationService class

--- Demo/src/main/java/ProductApp/Demo/Services/RegistrationService.java
+++ Demo/src/main/java/ProductApp/Demo/Services/RegistrationService.java
@@ -1,4 +1,16 @@
 package ProductApp.Demo.Services;
 
+import ProductApp.Demo.Entities.EventRegistration;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class RegistrationService {
+    Map<Integer, EventRegistration> registrations=new HashMap<>();
+
+    public RegistrationService(){
+        registrations.put(101, new EventRegistration("E1-01", "Ahmed"));
+        registrations.put(102, new EventRegistration("E2-02", "Sara"));
+        registrations.put(103, new EventRegistration("E3-03", "John"));
+    }
 }
