b6907c6d92cbfd17c434bd3f06aad34e8ed77c4f
wejdan al-subhi <alwejdan97@gmail.com>
2026-06-07 15:27:44 +0400

    adding getter and setter methods for all fields

--- Demo/src/main/java/ProductApp/Demo/LibraryMember.java
+++ Demo/src/main/java/ProductApp/Demo/LibraryMember.java
@@ -10,4 +10,28 @@ public class LibraryMember {
         this.fullName = fullName;
         this.membershipType = membershipType;
     }
+
+    public int getMemberId() {
+        return memberId;
+    }
+
+    public void setMemberId(int memberId) {
+        this.memberId = memberId;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getMembershipType() {
+        return membershipType;
+    }
+
+    public void setMembershipType(String membershipType) {
+        this.membershipType = membershipType;
+    }
 }
