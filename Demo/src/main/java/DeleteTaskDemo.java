f96d0549c57562aee988e41aaecad032c38e89c2
wejdan al-subhi <alwejdan97@gmail.com>
2026-06-07 15:21:37 +0400

    adding code for deleting in DeleteTaskDemo class

--- Demo/src/main/java/DeleteTaskDemo.java
+++ Demo/src/main/java/DeleteTaskDemo.java
@@ -7,7 +7,25 @@ public class DeleteTaskDemo {
         tasks.add("Call doctor");
         tasks.add("Finish report");
 
-        System.out.println("Available Tasks");
+        System.out.println("*** Show Available Tasks ***");
+        for(String t:tasks){
+            System.out.println(" "+t);
+        }
+
+        String taskToDelete="Buy groceries";
+        //boolean taskFound=false;
+        for(int i=0;i<tasks.size();i++){
+            if(tasks.get(i).equals(taskToDelete)){
+                tasks.remove(taskToDelete);
+               // taskFound=true;
+                System.out.println(taskToDelete+" Removed SUCCESSFULLY...");
+                break;
+            }else{
+                System.out.println(taskToDelete+" NOT Found...");
+            }
+        }
+
+        System.out.println("*** Show Available Tasks After Deleting ***");
         for(String t:tasks){
             System.out.println(" "+t);
         }
